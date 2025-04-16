05821f9e4d4fae17fc86128d943ab4b0d259d1b7
Karin Abe <karin.abe@gmail.com>
2025-04-15 22:42:17 -0300

    tabela de usuario  atualizada

--- api/app.py
+++ api/app.py
@@ -19,22 +19,23 @@ db = SQLAlchemy()
 # Define o modelo Usuario
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(80), unique=True, nullable=False)
     nome = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return f'<Usuario {self.nome}>'
+        return f'<Usuario {self.usuario}>'
 
     def json(self):
-        return {'id': self.id, 'nome': self.nome, 'email': self.email, 'senha': self.senha}
+        return {'id': self.id, 'usuario': self.usuario, 'nome': self.nome, 'email': self.email, 'senha': self.senha}
 
 def create_app():
     app = Flask(__name__)
 
     # Configura a URI do banco de dados para o SQLAlchemy
     app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}:{DATABASE_CONFIG['port']}/{DATABASE_CONFIG['database']}"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Para evitar avisos
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
     app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY', 'super-secret') # Configure a chave secreta JWT
     jwt = JWTManager(app)
@@ -51,20 +52,20 @@ def create_app():
         if request.method == 'GET':
             return render_template('index.html')
         elif request.method == 'POST':
-            nome = request.json.get('nome')
+            usuario = request.json.get('usuario')
             senha = request.json.get('senha')
-            if not nome or not senha:
-                return jsonify({'msg': 'nome e senha são obrigatórios'}), 400
+            if not usuario or not senha:
+                return jsonify({'msg': 'usuario e senha são obrigatórios'}), 400
 
             # Buscar o usuário no banco de dados pelo username
-            user = Usuario.query.filter_by(nome=nome).first()
+            user = Usuario.query.filter_by(usuario=usuario).first()
 
             if not user:
                 return jsonify({'msg': 'Usuário não existe'}), 401
 
             # Verificar se a senha fornecida corresponde ao hash armazenado no banco de dados
             if bcrypt.verify(senha, user.senha):
-                access_token = create_access_token(identity=nome)
+                access_token = create_access_token(identity=usuario)
                 return jsonify(access_token=access_token), 200
                 #return render_template('sucesso.html')
             else:
@@ -76,6 +77,9 @@ def create_app():
     def create_user():
         try:
             data = request.get_json()
+            usuario = data['usuario']
+            if Usuario.query.filter_by(usuario=usuario).first():
+                return jsonify({'message': 'Este usuario já existe!'}), 409
             nome = data['nome']
             email = data['email']
             senha = data['senha']
@@ -83,10 +87,10 @@ def create_app():
             # Hashear a senha            
             hashed_password_str = bcrypt.hash(senha)
             
-            new_user = Usuario(nome=nome, email=email, senha=hashed_password_str)
+            new_user = Usuario(usuario=usuario, nome=nome, email=email, senha=hashed_password_str)
             db.session.add(new_user)
             db.session.commit()
-            return jsonify({'message': 'user created'}), 201
+            return jsonify({'message': 'Usuário criado'}), 201
         except KeyError as e:
             db.session.rollback()
             logging.error(f"Erro ao criar usuário (dados ausentes): {e}")
@@ -94,15 +98,15 @@ def create_app():
         except Exception as e:
             db.session.rollback()
             logging.error(f"Erro ao criar usuário: {e}")
-            return jsonify({'message': 'error creating user'}), 500
+            return jsonify({'message': 'Erro ao criar usuário'}), 500
            
 # ESTE TRECHO NÃO ESTÁ FUNCIONANDO AINDA
     @app.route('/protegido', methods=['GET'])
     @jwt_required()
     def protegido():
         current_user = get_jwt_identity()
-        return render_template('sucesso.html')
-        # return jsonify(logado_como=current_user), 200
+        #return render_template('sucesso.html')
+        return jsonify(logado_como=current_user), 200
 
     # Lista todos os usuários
     @app.route('/usuario', methods=['GET'])
@@ -112,7 +116,20 @@ def create_app():
             return jsonify([usuario.json() for usuario in users]), 200
         except Exception as e:
             logging.error(f"Erro ao obter usuários: {e}")
-            return jsonify({'message': 'error getting users'}), 500
+            return jsonify({'message': 'Erro ao obter usuários'}), 500
+        
+ # Lista usuário por ID
+    @app.route('/usuario/<id>', methods=['GET'])
+    def get_user_by_id(id):
+        try:
+            users = Usuario.query.get(id)
+            if users:
+               return jsonify(users.json()), 200
+            else:
+                return jsonify({'message': 'usuario não encontrado'}), 404
+        except Exception as e:
+            logging.error(f"Erro ao obter usuários com ID {id}: {e}")
+            return jsonify({'message': 'erro ao obter usuario'}), 500
 
     # Atualizar
     @app.route("/usuario/<int:id>", methods=["PUT"])
@@ -122,6 +139,8 @@ def create_app():
             body = request.get_json()
             if body:
                 try:
+                    if 'usuario' in body:
+                        usuario_objeto.usuario = body['usuario']
                     if 'nome' in body:
                         usuario_objeto.nome = body['nome']
                     if 'email' in body:
@@ -131,14 +150,14 @@ def create_app():
                         usuario_objeto.senha = bcrypt.hash(body['senha'])
 
                     db.session.commit()
-                    return jsonify({'message': 'usuario updated successfully', 'usuario': usuario_objeto.json()}), 200
+                    return jsonify({'message': 'Usuário atualizado com sucesso', 'usuario': usuario_objeto.json()}), 200
                 except Exception as e:
                     print('Erro', e)
                     db.session.rollback()
-                    return jsonify({'message': 'error updating user'}), 400
+                    return jsonify({'message': 'Erro ao atualizar usuário'}), 400
             else:
-                return jsonify({'message': 'request body is empty'}), 400
-        return jsonify({'message': 'user not found'}), 404
+                return jsonify({'message': 'Campo vazio'}), 400
+        return jsonify({'message': 'Usuário não encontrado'}), 404
 
     # Deletar
     @app.route("/usuario/<int:id>", methods=["DELETE"])
@@ -148,12 +167,12 @@ def create_app():
             try:
                 db.session.delete(usuario_objeto)
                 db.session.commit()
-                return jsonify({'message': 'usuario deleted successfully'}), 200
+                return jsonify({'message': 'usuario removido com sucesso'}), 200
             except Exception as e:
                 print('Erro', e)
                 db.session.rollback()  # Em caso de erro, desfaz as alterações na sessão
-                return jsonify({'message': 'error deleting user'}), 400
-        return jsonify({'message': 'user not found'}), 404
+                return jsonify({'message': 'erro ao remover usário'}), 400
+        return jsonify({'message': 'usuário não encontrado'}), 404
 
     return app
 
