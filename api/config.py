f42ee5949ca064276b2c8551c5013e2f44ba1e61
Karin Abe <karin.abe@gmail.com>
2025-05-31 21:48:23 -0300

    Delete config.py

--- api/config.py
+++
@@ -1,7 +0,0 @@
-DATABASE_CONFIG = {
-    "database": "neondb",
-    "host": "ep-crimson-dust-a56j8j1s-pooler.us-east-2.aws.neon.tech",
-    "user": "neondb_owner",
-    "password": "npg_Np8jgnok3rhZ",
-    "port": "5432",
-}
